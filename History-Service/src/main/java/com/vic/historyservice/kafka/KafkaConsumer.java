a3c1e858e52dd11283134a889a6f913987130ebe
Vicktor007 <vicktord007@gmail.com>
2025-11-17 08:15:56 +0100

    requeing fixed

--- History-Service/src/main/java/com/vic/historyservice/kafka/KafkaConsumer.java
+++ History-Service/src/main/java/com/vic/historyservice/kafka/KafkaConsumer.java
@@ -62,7 +62,7 @@ public class KafkaConsumer {
             acknowledgment.acknowledge();
         } catch (Exception e) {
             log.error("Error while saving wallet event notification :: {}", walletEvent, e);
-            throw e;
+            throw e; // triggers retry topic
         }
     }
 
